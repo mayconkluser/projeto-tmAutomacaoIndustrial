7eabede4b435708131460eb6dd032870f8f87f89
Maycon Klüser <maycon10kluser@gmail.com>
2022-08-29 23:51:43 -0300

    criado .js tabMenu na automacao.html

---
+++ js/module/tab-menu-automation.js
@@ -0,0 +1,27 @@
+export default function initTabMenuAutomation() {
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+  const active = "active";
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(active);
+    tabMenu[0].classList.add(active);
+
+    function activeTab(index) {
+      tabContent.forEach((item) => {
+        item.classList.remove(active);
+      });
+      tabMenu.forEach((item) => {
+        item.classList.remove(active);
+      });
+      tabMenu[index].classList.add(active);
+      tabContent[index].classList.add(active);
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initMenuMobile from "./module/menu-mobile.js";
 import initTopPage from "./module/top-page.js";
+import initTabMenuAutomation from "./module/tab-menu-automation.js";
 
 initMenuMobile();
 initTopPage();
+initTabMenuAutomation();
