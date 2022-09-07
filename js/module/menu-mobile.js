49a05fae192c96d19c947768df1e6b8098e86136
Maycon Klüser <maycon10kluser@gmail.com>
2022-09-07 01:08:23 -0300

    ajustados menu mobile e adicionado favicon

--- js/module/menu-mobile.js
+++ js/module/menu-mobile.js
@@ -2,10 +2,22 @@ export default function initMenuMobile() {}
 
 const nav = document.querySelector('[data-menu="btnMenu"]');
 const ulMenu = document.querySelector('[data-menu="ulMenu"]');
+const activeLinksWidth = document.querySelectorAll(".ul-menu li a");
 const active = "active";
 
 function openMenu(event) {
   nav.classList.toggle(active);
   ulMenu.classList.toggle(active);
 }
+
+function activeLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add(active);
+  }
+}
+
+activeLinksWidth.forEach(activeLink);
 nav.addEventListener("click", openMenu);
